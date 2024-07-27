631bbf11c6ef9a9e1612868de6354f71b9de0984
KENMEGNE YOH IVANA <125506456+yohivana-KYIM@users.noreply.github.com>
2024-07-27 19:35:16 +0100

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import removeConsole from "vite-plugin-remove-console";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react(), mode === "production" && removeConsole()].filter(Boolean),
+//   server: {
+//     host: true,
+//     strictPort: true,
+//     port: 8080,
+//     proxy: {
+//       "/api/v1": {
+//         target:"https://davincitsolutions-expense-api.onrender.com",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// }));
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import removeConsole from "vite-plugin-remove-console";
@@ -11,33 +32,9 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api/v1": {
-        target:"https://davincitsolutions-expense-api.onrender.com",
+        target: "https://davincitsolutions-expense-api.onrender.com",
         changeOrigin: true,
       },
     },
   },
 }));
-
-
-
-
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import removeConsole from "vite-plugin-remove-console";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   plugins: [react(), mode === "production" && removeConsole()].filter(Boolean),
-//   server: {
-//     host: true,
-//     strictPort: true,
-//     port: 8080,
-//     proxy: {
-//       "/api": {
-//         target:"https://davincitsolutions-expense-api.onrender.com/api/v1",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// }));
