e3725a63137c6f7a4b97b585834d84a837cee3e5
KENMEGNE YOH IVANA <125506456+yohivana-KYIM@users.noreply.github.com>
2024-07-27 10:08:22 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,33 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api/v1": {
-        target:"https://expense-davincitsolutions-api.onrender.com",
+        target:"https://davincitsolutions-expense-api.onrender.com",
         changeOrigin: true,
       },
     },
   },
 }));
+
+
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import removeConsole from "vite-plugin-remove-console";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react(), mode === "production" && removeConsole()].filter(Boolean),
+//   server: {
+//     host: true,
+//     strictPort: true,
+//     port: 8080,
+//     proxy: {
+//       "/api": {
+//         target:"https://davincitsolutions-expense-api.onrender.com/api/v1",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// }));
