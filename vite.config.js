cca04973252ee3e4bf6c0381defee4ba4ca01f87
KENMEGNE YOH IVANA <125506456+yohivana-KYIM@users.noreply.github.com>
2024-07-28 01:14:58 +0100

    deplo

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,22 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import removeConsole from "vite-plugin-remove-console";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import removeConsole from "vite-plugin-remove-console";
 
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   plugins: [react(), mode === "production" && removeConsole()].filter(Boolean),
-//   server: {
-//     host: true,
-//     strictPort: true,
-//     port: 8080,
-//     proxy: {
-//       "/api/v1": {
-//         target:"https://davincitsolutions-expense-api.onrender.com",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// }));
-
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), mode === "production" && removeConsole()].filter(Boolean),
   server: {
+    host: true,
+    strictPort: true,
+    port: 8080,
     proxy: {
-      '/api/v1': {
-        target: 'https://davincitsolutions-expense-api.onrender.com',
+      "/api/v1": {
+        target:"https://davincitsolutions-expense-api.onrender.com",
         changeOrigin: true,
-        secure: false,
       },
     },
   },
-});
+}));
 
 
 
